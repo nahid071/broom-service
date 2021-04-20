c4fd21d01de9f1a3e45718d9840c49e03c22f374
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-04-20 10:30:01 +0600

    pages route decoration

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,16 @@ router.get("/approve", (req, res) => {
   res.render("approve");
 });
 
+router.get("/profile-booking", (req, res) => {
+  res.render("profile-booking");
+});
+
 router.get("/request", (req, res) => {
   res.render("request");
+});
+
+router.get("/services", (req, res) => {
+  res.render("services");
 });
 
 module.exports = router;
