4b9c4c8237ca60262d2f52e92bb959ba05b0f8d8
Alamgir Ahamed Saif <36258593+alamgir72435@users.noreply.github.com>
2021-04-25 19:48:45 +0600

    pagination

--- routes/index.js
+++ routes/index.js
@@ -93,7 +93,49 @@ router.get("/request", (req, res) => {
   res.render("request");
 });
 
-router.get("/services", (req, res) => {
+function paginatedResults(model) {
+  return async (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < (await model.countDocuments().exec())) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+    try {
+      results.results = await model.find().limit(limit).skip(startIndex).exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
+}
+
+router.get(
+  "/service-contractors",
+  paginatedResults(Contractor),
+  async (req, res) => {
+    //const contractors = await Contractor.find().lean().populate("ratings");
+    res.json(res.paginatedResults);
+  }
+);
+
+router.get("/services", async (req, res) => {
   res.render("services");
 });
 
